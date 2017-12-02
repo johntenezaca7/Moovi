b4b0a2b5e853c559c627acb9eebd71bb5309620c
Owner <owner@MacBook-Pro.local>
2017-12-02 10:52:38 -0500

    added an enviroment check to render create react app in prod

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.send({hey:"Hello"})
 })
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log('Server started at ' + PORT);
